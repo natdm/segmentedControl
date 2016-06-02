802d3010c49660098cd16422b36059e44a4e1793
Nathan Hyland <nhyland@NathanHyland.local>
2016-06-02 12:15:10 -0700

    Updated files being called per platform

---
+++ segmentedControlMain.js
@@ -0,0 +1,10 @@
+import {Platform} from 'react-native';
+import SCAndroid from './segmentedControl.android';
+import SCiOS from './segmentedControl.ios';
+
+const Component = p =>
+    Platform.OS == 'android'
+        ? <SCAndroid {...p} />
+        : <SCiOS {...p} />;
+
+export default Component;
